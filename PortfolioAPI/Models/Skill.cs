f1ec73e603e4761a993e6776c0244d8eba951bdc
jpucher <jakob.pucher@hiq.se>
2024-11-06 14:50:02 +0100

    init

---
+++ PortfolioAPI/Models/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace PortfolioAPI.Models
+{
+    public class Skill
+    {
+        public  int? id { get; set; }
+        public required string name { get; set; }
+        public required string icon { get; set; }
+    }
+}
---
+++ PortfolioAPI/Program.cs
@@ -0,0 +1,47 @@
+using Dapper;
+using PortfolioAPI.Models;
+using System.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/skills", async  () =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    using var connection = new SqlConnection(connectionString);
+    var skills = await connection.QueryAsync<Skill>("SELECT id, name, icon FROM skills");
+
+    return skills;
+});
+
+
+app.MapPost("/skills", async (Skill skill) =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    using var connection = new SqlConnection(connectionString);
+    const string insertQuery = "INSERT INTO skills (name, icon) VALUES (@name, @icon);";
+
+    var result = await connection.ExecuteAsync(insertQuery, new { skill.name, skill.icon });
+
+    return result > 0 ? Results.Ok("Skill added successfully") : Results.Problem("Failed to add skill.");
+});
+
+app.Run();
